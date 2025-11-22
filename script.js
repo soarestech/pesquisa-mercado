ec3a16c3c5a72510609152e23283ed01626df68c
soarestech <zf62shop@gmail.com>
2025-11-21 21:26:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ apagarTudoBtn.addEventListener('click', () => {
         conteudo += '------------------------------------------------------\n';
 
         // Cabeçalho da "tabela"
-        conteudo += 'Produto'.padEnd(20) + 'Embalagem'.padEnd(15) + 'Valor (R$).padEnd(15) + '\n';
+        conteudo += 'Produto'.padEnd(20) + 'Embalagem'.padEnd(18) + 'Preço (R$)\n';
 
         // Itens da lista: atenção aos nomes das propriedades (nome, embalagem, valor)
         (listaObj.produtos || []).forEach(item => {
@@ -238,10 +238,10 @@ apagarTudoBtn.addEventListener('click', () => {
           const embal = (item.embalagem || '').toString();
           const valorStr = (typeof item.valor !== 'undefined') ? parseFloat(item.valor).toFixed(2) : '';
 
-          conteudo += nomeProduto.padEnd(20) + embal.padEnd(15) + valorStr.padEnd(15) + '\n';
+          conteudo += nomeProduto.padEnd(25) + embal.padEnd(18) + valorStr.padEnd(1) + '\n';
         });
 
-        conteudo += '\n==============================================\n\n';
+        conteudo += '\n===================================================\n\n';
       });
 
       // Cria e baixa o arquivo
@@ -256,3 +256,4 @@ apagarTudoBtn.addEventListener('click', () => {
   })();
 
 });
+
